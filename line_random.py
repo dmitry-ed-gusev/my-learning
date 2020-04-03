369075400ca7e9e587a1f3ac660f40d0c7a2b1f7
DiMAS256 <mac_98@bk.ru>
2020-04-03 15:58:03 +0300

    Update line_random.py

--- line_random.py
+++ line_random.py
@@ -9,19 +9,24 @@
 
 from random import random
 
-N = 7 # колличество значений в списке
- 
-def average(a): # среднее значение а
-    s = sum(a) # встроенная функция sum(), которая возвращает сумму элементов переданного ей списка
-    return s/N 
- 
-arr = [0] * N
-for i in range(N):
-    arr[i] = (random() * 1) # задается промежуток до 1
+n = int(input("Введите необходимое колличество чисел: ")) # колличество значений в списке
 
-f = min(arr)
-z = max (arr)
-b = average(arr)
-print(arr)
-print ('\n')
-print(f, b, z)
+if n > 0:
+
+	def average(a): # среднее значение а
+		s = sum(a) # встроенная функция sum(), которая возвращает сумму элементов переданного ей списка
+		return s/n 
+ 
+	arr = [0] * n
+	for i in range(n):
+		arr[i] = (random() * 1) # задается промежуток до 1
+
+	f = min(arr)
+	z = max(arr)
+	b = average(arr)
+	print(arr)
+	print ('\n')
+	print(f, b, z)
+
+else:
+	print ("Необходимо ввести число значей.")
