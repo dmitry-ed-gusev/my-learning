80300311a9c211d91eaed345817af6096e994be1
Dmitrii Gusev <dmitry.ed.gusev@gmail.com>
2020-04-12 19:54:15 +0200

    Added module my_utilities. Added breakdown into functions for line_random.

--- line_random.py
+++ line_random.py
@@ -3,63 +3,49 @@
 #	Simple math #1
 #
 #	Created:  Ishkinin Dmitrii, 17.03.2020
-#	Modified: 
+#	Modified: Dmitrii Gusev, 12.04.2020
 #
 ################################################################################
 
 from random import random
-import sys # импортируем библиотеку
+import sys  # импортируем библиотеку
+from utilities.my_utilities import average
+
+# -- функции
 
 
-text = len(sys.argv) # длинна аргументов (0, если ввести значение, то: 1, ...)
+def init_array(arr_length):
+    arr = [0] * arr_length
+    for i in range(arr_length):
+        arr[i] = (random() * 1)  # задается промежуток до 1
+    return arr
 
-if text >= 2: # если значение text > 1
 
-	n = int(sys.argv [1]) # меняю значение переменной
+def print_result(arr, minimum, maximum, average_value):
+    print('массив: ', arr, '\n')
+    print('минимум: ', minimum)
+    print('среднее: ', average_value)
+    print('максимум: ', maximum)
 
-	def average(a): # среднее значение а
-		s = sum(a) # встроенная функция sum(), которая возвращает сумму элементов переданного ей списка
-		return s/n 
 
-	arr = [0] * n
-	for i in range(n):
-		arr[i] = (random() * 1) # задается промежуток до 1
+def worker(value):
+    arr_length = int(value)  # приводим значение к целочисленному
+    arr = init_array(arr_length)  # инициализация массива
+    print_result(arr, min(arr), max(arr), average(arr))  # печать результатов
 
-	f = min(arr)
-	z = max(arr)
-	b = average(arr)
 
-	print(arr)
-	print('\n')
-	print(f, b, z)
+def main_function(args_length):
+    if args_length >= 2:  # если значение text > 1
+        worker(sys.argv[1])
+    elif args_length == 1:
+        gav = input("Введите необходимую длинну массива: ")
+        m = len(gav)
+        if m >= 1:
+            worker(gav)
+        else:
+            print("Необходимо ввести число!")
 
-elif text == 1:
 
-	gav = input ("Введите необходимую длинну массива: ")
-	
-	m = len(gav)
-
-	if m >= 1:
-
-		n = int(gav)
-
-		def average(a): # среднее значение а
-			s = sum(a) # встроенная функция sum(), которая возвращает сумму элементов переданного ей списка
-			return s/n
-
-		arr = [0] * n
-		for i in range(n):
-			arr[i] = (random() * 1) # задается промежуток до 1
-
-		f = min(arr)
-		z = max(arr)
-		b = average(arr)
-
-		print(arr)
-		print('\n')
-		print(f, b, z)
-
-	else:
-	
-		print ("Необходимо ввести число!")
-
+# -- код скрипта / программы
+length = len(sys.argv)  # длинна аргументов (0, если ввести значение, то: 1, ...)
+main_function(length)
