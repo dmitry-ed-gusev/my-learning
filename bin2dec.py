704f475a3dc714ca4e7e6f6f78d00cfc0a5aa707
DiMAS256 <mac_98@bk.ru>
2020-05-02 20:21:51 +0300

    new

--- bin2dec.py
+++ bin2dec.py
@@ -9,16 +9,17 @@
 
 import sys # импортируем библиотеку
 
-text = len(sys.argv) # длинна аргументов (0, если ввести значение, то: 1, ...)
 
-if text > 1: # если значение text > 1
+def calculate_sum(tab):
+	
+	if tab > 1:
+		text = (sys.argv [1]) # меняю значение переменной
+		cool = int(text, 2)
+		print (cool)
+	else:
+		print ("Please, provide a binary number for conversion...")
 
-	text = (sys.argv [1]) # меняю значение переменной
 
-	cool = int(text, 2) # int - перевод в десятичную, .., 2 перевод из двоичной
-
-	print (cool)
-
-else:
-
-	print ("Please, provide a binary number for conversion...")
+# -- код скрипта / программы
+hold = len(sys.argv)
+calculate_sum(hold)
--- dec2bin.py
+++ dec2bin.py
@@ -9,18 +9,18 @@
 
 import sys # импортируем библиотеку
 
-text = len(sys.argv) # длинна аргументов (0, если ввести значение, то: 1, ...)
 
-if text > 1: # если значение text > 1
-
-	text = (sys.argv [1]) # меняю значение переменной
-
-	cool = (float(text))
-
-	print (bin(int(cool))) # печать, двоичный код (если число целое),  , удаляем 2 последних символа
+def calculate_sum(tab):
 	
-else:
+	if tab > 1:
+		text = (sys.argv [1]) # меняю значение переменной
+		cool = (float(text))
+		print (bin(int(cool))) # печать, двоичный код (если число целое),  , удаляем 2 последних символа
+	else:
+		print ("Please, provide a decimal number for conversion...")
 
-	print ("Please, provide a decimal number for conversion...")
 
-	
+
+# -- код скрипта / программы
+hold = len(sys.argv)  # hold равен длинне кортежа
+calculate_sum(hold)  # со значением hold выполняется вся функция
